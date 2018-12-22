049849a46bf209fd939540a001a72b418af366ef
Kay Johansen <kay-johansen@pluralsight.com>
2018-12-21 17:00:07 -0700

    completed next module

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,14 +10,23 @@ class Quiz extends Component {
         super(props)
         this.state = { quiz_position: 1}
     }    
-
+    showNextQuestion() {
+        this.setState({
+            ...this.state,
+            quiz_position: this.state.quiz_position + 1
+        })
+    }
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
+        console.log(quizData.quiz_questions[this.state.quiz_position-1])
         return <div>
             {isQuizEnd ? 
-            <QuizEnd />
+                <QuizEnd />
             :
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                <QuizQuestion 
+                    quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+                    showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                />
             }
         </div>
     }
